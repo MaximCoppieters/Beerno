5c71d45a786ae5e871a91df25338437a8050273c
Michael-Meeus <Michael.Meeus@gmail.com>
2019-02-07 11:37:05 +0100

    Additions to BeerRouteActivity etc

--- app/src/main/java/be/pxl/beerno/MainActivity.java
+++ app/src/main/java/be/pxl/beerno/MainActivity.java
@@ -1,5 +1,6 @@
 package be.pxl.beerno;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Start(View view) {
+        Intent intent = new Intent(MainActivity.this, BeerRouteActivity.class);
+        startActivity(intent);
     }
 }
