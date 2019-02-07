d03b870e893e95be9ec029af8fa102fdf80c5049
SachaKleynen <11701347@student.pxl.be>
2019-02-07 10:36:13 +0100

    beers added final

--- app/src/main/java/be/pxl/beerno/BeerRepository.java
+++ app/src/main/java/be/pxl/beerno/BeerRepository.java
@@ -11,12 +11,17 @@ public class BeerRepository {
         List<Beer> beers = new ArrayList<>();
 
 
-        Beer stella = new Beer("Stella Artois", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\stella.png");
-        Beer jupiler = new Beer("Jupiler", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\jupiler.png");
+        Beer stella = new Beer("Stella Artois", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\stella.png"));
+        Beer jupiler = new Beer("Jupiler", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\jupiler.png"));
         Beer guiness = new Beer("Guiness", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\guiness.jpg"));
+        Beer krusuvice = new Beer("Krušovice", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\krusovice.png"));
+        Beer heineken = new Beer("Heineken", Paths.get("Beerno\\app\\src\\main\\res\\drawable-xxxhdpi\\heineken.png"));
 
         beers.add(jupiler);
         beers.add(stella);
+        beers.add(guiness);
+        beers.add(krusuvice);
+        beers.add(heineken);
 
         return beers;
     }
