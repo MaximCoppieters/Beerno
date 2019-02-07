5ace525eaab5bbf8e1d6a7c04ba92e49e3e73158
Michael-Meeus <Michael.Meeus@gmail.com>
2019-02-07 22:58:57 +0100

    Finish button zorgt nu voor redirect terug naar begin select scherm

--- app/src/main/java/be/pxl/beerno/SummaryActivity.java
+++ app/src/main/java/be/pxl/beerno/SummaryActivity.java
@@ -1,13 +1,17 @@
 package be.pxl.beerno;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class SummaryActivity extends AppCompatActivity {
+
+    private CardView finishCardview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +19,13 @@ public class SummaryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_summary);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        finishCardview = findViewById(R.id.finish);
+        finishCardview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SummaryActivity.this, BeerSelectActivity.class));
+            }
+        });
     }
 
 
