388bef966ecc60553df1906e97e020a7f26d3cdf
11700747 <11700747@student.pxl.be>
2019-02-07 14:38:00 +0100

    Applied beer changes

--- app/src/main/java/be/pxl/beerno/BeerSelectActivity.java
+++ app/src/main/java/be/pxl/beerno/BeerSelectActivity.java
@@ -29,15 +29,6 @@ public class BeerSelectActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         initializeBeerList();
     }
 
