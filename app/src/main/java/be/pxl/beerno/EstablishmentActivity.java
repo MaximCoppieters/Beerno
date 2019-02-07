83e0602fc36a23e2e4d7e863670f9b0a03ee91a5
Michael-Meeus <Michael.Meeus@gmail.com>
2019-02-07 19:41:00 +0100

    Establishment works now, Not all beers and images are loaded yet. Resume button works now.

--- app/src/main/java/be/pxl/beerno/EstablishmentActivity.java
+++ app/src/main/java/be/pxl/beerno/EstablishmentActivity.java
@@ -1,14 +1,20 @@
 package be.pxl.beerno;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
@@ -18,7 +24,10 @@ public class EstablishmentActivity extends AppCompatActivity {
     private TextView establishmentName;
     private Button incrementButton;
     private Button decrementButton;
+    private CardView resumeButton;
     private Establishment establishment;
+    private ImageView beer_image;
+    private List<Beer> beerList;
 
 
     @Override
@@ -29,8 +38,16 @@ public class EstablishmentActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         incrementButton = findViewById(R.id.increment_button);
         decrementButton = findViewById(R.id.decrement_button);
+        resumeButton = findViewById(R.id.resume);
         establishmentName = findViewById(R.id.establishment_name);
         //establishmentName.setText(establishment.getName());
+        beerList = BeerRepository.getAllBeers();
+        beer_image = findViewById(R.id.beer_image);
+        for (Beer beer : beerList
+             ) {
+            beer_image.setImageResource(beer.getImageId());
+            Picasso.get().load(beer.getImageId()).into(beer_image);
+        }
 
         incrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,10 +55,18 @@ public class EstablishmentActivity extends AppCompatActivity {
                 drinkCount++;
             }
         });
+
         decrementButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 drinkCount--;
+            }
+        });
+
+        resumeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EstablishmentActivity.this, BeerRouteActivity.class));
             }
         });
     }
