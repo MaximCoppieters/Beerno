54084b4efb934d130ab7db01ce1ae55914ffcf9d
SachaKleynen <11701347@student.pxl.be>
2019-02-08 10:24:06 +0100

    exit app when finish pressed

--- app/src/main/java/be/pxl/beerno/SummaryActivity.java
+++ app/src/main/java/be/pxl/beerno/SummaryActivity.java
@@ -25,7 +25,7 @@ public class SummaryActivity extends AppCompatActivity {
         finishCardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SummaryActivity.this, BeerSelectActivity.class));
+               System.exit(0);
             }
         });
 
